b2b6a1934faf7a22b8ac82ee76c2aef46b1b1b7c
kushall <canaby007@gmail.com>
2026-09-07 11:25:21 +0530

    fix: update CORS middleware to support credentialed requests from localhost and browser clients

--- main.py
+++ main.py
@@ -826,7 +826,15 @@ except Exception as e:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"https?://.*",
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
